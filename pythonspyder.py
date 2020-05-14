6f7465e3dda054fbd84bd7e42f3abcb5f9e09e14
RUBIO APARICIO <nrubio@academia.usbbog.edu.co>
2020-05-14 02:07:48 -0500

    nuevo archivo en spyder

---
+++ pythonspyder.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 02:04:07 2020
+
+@author: RUBIOAPARICIO
+"""
+
+
+print('new python script')
+
+# %%
+"""
+este es un cambio de linea para compilar por secicones en pyton
+"""
+
+print('hola mundo')
+
